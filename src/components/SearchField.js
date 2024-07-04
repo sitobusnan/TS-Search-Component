1d2f03594c1213e3b84d43ceb64bd281cda92916
FrontGeorge <jorgelopezverdu@gmail.com>
2024-07-04 14:32:35 +0200

    new search

---
+++ src/components/SearchField.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Search.css'; // Opcional, para estilos
+
+const SearchField = () => {
+  return (
+    <>
+      <label for="search" className="search">
+        <input type="search" id="search"  />
+        <button className='search-button' aria-label="buscar">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </label>
+      <div className="search--icon">
+        <i class="fa-solid fa-microphone"></i>
+      </div>
+    </>
+  );
+};
+
+export default SearchField;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Search.css'; // Opcional, para estilos
+import SearchField from '../components/SearchField';
 
 const Search = () => {
   return (
-    <div className="login">
-      <h1>Bienvenido a la Página de Search</h1>
-      <p>Esta es la página principal de la aplicación.</p>
-    </div>
+    <section className="search">
+    <header className='search__header'>
+      <div className='search__header--wrapper'>
+        <img src="public/assets/teamSystemlogo.svg" alt="" />
+        <h1 className='search__header--title'>TeamSystem <span>Datahub</span></h1>
+        <div className='search--wrapper'>
+          <i class="fa-regular fa-bell"></i>
+        </div>
+      </div>
+      <div className='search__header--search'>
+        <SearchField />   
+      </div>
+    </header>
+    <main>
+    </main>
+    <footter>
+
+
+    </footter>
+    </section>
   );
 };
 
