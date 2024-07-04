7e821be32d8823521f7ab5a1da588d403a96f6c3
jga-dev <j.garcia@teamsystem.com>
2024-07-04 17:18:58 +0200

    Pasand props

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './SearchItem.css'; // Opcional, para estilos
 
-const SearchItem = () => {
+const SearchItem = ({ type, client }) => {
   return (
     <li className='search__item'>
       <div className="search__item--icon"></div>
       <div className="search__item--content">
-        <p className='search__item--title'>Title</p>
+        <p className='search__item--title'>{client.name}</p>
         <p className='search__item--subtitle'>Subtitle</p>
         <p className='search__item--info'>Info</p>
       </div>
