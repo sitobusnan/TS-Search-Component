43aa66377d8aa5227c7863fe472b0a27b3359750
FrontGeorge <jorgelopezverdu@gmail.com>
2024-07-04 11:27:27 +0200

    skafolding

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'; // Opcional, para estilos
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Bienvenido a la Página de Login</h1>
+      <p>Esta es la página principal de la aplicación.</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.css'; // Opcional, para estilos
+
+const Search = () => {
+  return (
+    <div className="login">
+      <h1>Bienvenido a la Página de Search</h1>
+      <p>Esta es la página principal de la aplicación.</p>
+    </div>
+  );
+};
+
+export default Search;
